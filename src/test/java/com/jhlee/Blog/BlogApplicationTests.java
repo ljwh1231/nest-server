b3abfbeebd807773690f212db0d388ab00e2495e
ljwh1231 <ljwh1231@yonsei.ac.kr>
2022-01-20 18:34:15 +0900

    fix : datasource 연결 속성 수정

--- src/test/java/com/jhlee/Blog/BlogApplicationTests.java
+++ src/test/java/com/jhlee/Blog/BlogApplicationTests.java
@@ -3,7 +3,7 @@ package com.jhlee.Blog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BlogApplicationTests {
 
 	@Test
